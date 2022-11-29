479fd9d74943a0c7ea2a8398e49223e1ff7fd017
flappykled <dima04089804@gmail.com>
2022-11-29 19:12:15 +0200

    homework

--- js/task-02.js
+++ js/task-02.js
@@ -1,20 +1,13 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments']
 
 const list = document.querySelector('#ingredients')
 const liArray = []
 
-ingredients.forEach(ingredients => {
-  const item = document.createElement('li')
-  item.className = 'item'
-  item.textContent = ingredient
-  liArray.push(item)
+ingredients.forEach(ingredient => {
+	const item = document.createElement('li')
+	item.className = 'item'
+	item.textContent = ingredient
+	liArray.push(item)
 })
 
 list.append(...liArray)
